f4177fe49ce2b38b4ef69a0ca75f73b55a1522af
alienx456 <eeromero@correo.udistrital.edu.co>
2019-06-02 21:02:15 -0500

    Added one component per page -last commit didnt added them

---
+++ CineDistrito-cli/src/app/pages/confiteria/confiteria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confiteria',
+  templateUrl: './confiteria.component.html',
+  styleUrls: ['./confiteria.component.scss']
+})
+export class ConfiteriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
