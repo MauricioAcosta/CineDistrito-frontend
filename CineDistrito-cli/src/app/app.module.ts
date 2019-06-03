700d800d18d6132d12f234a2b267ad11f17069aa
alienx456 <eeromero@correo.udistrital.edu.co>
2019-06-02 21:00:44 -0500

    Added one component per page

--- CineDistrito-cli/src/app/app.module.ts
+++ CineDistrito-cli/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CarteleraComponent } from './pages/cartelera/cartelera.component';
+import { MultiplexComponent } from './pages/multiplex/multiplex.component';
+import { PromocionesComponent } from './pages/promociones/promociones.component';
+import { ConfiteriaComponent } from './pages/confiteria/confiteria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarteleraComponent,
+    MultiplexComponent,
+    PromocionesComponent,
+    ConfiteriaComponent
   ],
   imports: [
     BrowserModule,
