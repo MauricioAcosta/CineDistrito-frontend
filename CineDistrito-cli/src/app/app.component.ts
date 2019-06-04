c8773b4cf8423e0699004a22ea85c3784b1bf333
AlienX456 <estebanelias27@gmail.com>
2019-06-04 13:35:29 -0500

    Header and Footer done, stating with home component

--- CineDistrito-cli/src/app/app.component.ts
+++ CineDistrito-cli/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None //https://stackoverflow.com/questions/46670795/how-to-change-whole-page-background-color-in-angular
 })
 export class AppComponent {
   title = 'CineDistrito-cli';
