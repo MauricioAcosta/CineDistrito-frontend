15e72ca6c149adb04adcba81310b422ff10d2406
alienx456 <estebanelias27@gmail.com>
2019-06-30 14:31:54 -0500

    Graph of chairs is completed, it change colors with the status

--- CineDistrito-cli/src/app/componentes/pages/reservar/reservar.component.ts
+++ CineDistrito-cli/src/app/componentes/pages/reservar/reservar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 //servicio
-import {ObtenerInformacionPeliculaIMDbService} from '../../../servicios/obtener-informacion-pelicula-imdb.service'
+import {ObtenerInformacionPeliculaIMDbService} from '../../../servicios/obtener-informacion-pelicula-imdb.service';
 
 
 @Component({
@@ -13,9 +13,17 @@ export class ReservarComponent implements OnInit {
 
   public info_pelicula:any;
   public id_IMDb:any;
+  public seatsState:String[];
 
   constructor(private ObtenerInformacionPeliculaIMDbService:ObtenerInformacionPeliculaIMDbService) {
     this.id_IMDb = 'tt6108178';
+    this.seatsState = [];
+    let stateRand:Number;
+    for (let i=1; i < 61;i++)
+    {
+      stateRand = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+      this.seatsState.push(this.convertirNumeroAEstado(stateRand));
+    }
   }
 
   ngOnInit() {
@@ -29,7 +37,50 @@ export class ReservarComponent implements OnInit {
     );
   }
 
-  onClickMe() {
-    alert("You are my hero!");
+  convertirNumeroAEstado(numero:Number){
+    if (numero==1){
+      return "available";
+    }
+    else if (numero==2){
+      return "active";
+    }
+    else if (numero==3){
+      return "onprocess";
+    }
+    else if (numero==4){
+      return "reserved";
+    }
+    
   }
+
+  onClickMe() {
+    let stateRand:Number;
+    for (let i=1; i < 61;i++)
+    {
+      stateRand = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+      this.seatsState[i-1]=this.convertirNumeroAEstado(stateRand);
+    }
+  }
+
+  verificarestado_sillas(){
+
+  }
+
+  cambiarestadosilla_disponible(id){
+    
+  }
+
+  cambiarestadosilla_nodisponible(id){
+
+  }
+
+  cambiarestadosilla_enproceso(id){
+
+  }
+
+  cambiarestadosilla_seleccionada(id){
+
+  }
+
+
 }
