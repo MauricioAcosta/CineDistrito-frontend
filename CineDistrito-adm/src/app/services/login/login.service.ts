674c9af1c3db37aa4b199f195990eca5ae929a08
MauricioAcosta <andres.taamap@gmail.com>
2019-07-15 10:54:14 -0500

    delete modules not use

--- CineDistrito-adm/src/app/services/login/login.service.ts
+++ CineDistrito-adm/src/app/services/login/login.service.ts
@@ -1,43 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class LoginService {
-  data=[];
-  apiURL: string = environment.KEYCLOAK_URL;
-  httpHeaders = new HttpHeaders({
-    'Content-Type': 'application/x-www-form-urlencoded'
-  });
-  constructor(private httpClient: HttpClient, private route:Router) { 
+  url: string = environment.url;
+  private httpOptions;
+  constructor(private http: HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + btoa('username:password')
+      })
+    };
+  }
 
+  public login(username: string, password: string) {
+    return this.http.get<any>(`${this.url}/usuarios/personas`, this.headers);
   }
-  login(username: string, password: string) {
-    let parametros = new HttpParams()
-      .set("grant_type", "password")
-      .set("client_id", "CineDistrito")
-      .set("client_secret", "ef705ea9-754b-4178-bcd7-80920c0adb7d")
-      .set("username", username)
-      .set("password", password)
-  
-    return this.httpClient.post<any>(this.apiURL + '/realms/SAT/protocol/openid-connect/token', parametros, { headers: this.httpHeaders }).subscribe((res => {
-      this.data=JSON.parse(atob(res.access_token.split('.')[1]))
-      console.log(this.data);
-      sessionStorage.setItem('access_token',res.access_token);
-      this.route.navigate(['inicio']);
-    }),error => {
-      console.log("error", error);
-      
-    })
-  }
+
   logout() {
-    sessionStorage.removeItem('access_token');
-  }
-  public get loggedIn(): boolean {
-    return sessionStorage.getItem('access_token') !== null;
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
   }
 }
