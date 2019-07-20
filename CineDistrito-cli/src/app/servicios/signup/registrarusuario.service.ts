b8e2c2108751576d531e2434409afeafeb47b6af
alienx456 <estebanelias27@gmail.com>
2019-07-19 21:17:48 -0500

    Now a customer can register himself by using the singup modal

---
+++ CineDistrito-cli/src/app/servicios/signup/registrarusuario.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrarusuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  registrarUsuario(cedula,telefono,direccion,nusuario,email,nombres,apellidos,contrasena){
+
+    let myheaders = new HttpHeaders();
+    myheaders = myheaders.append("Content-Type", "application/json");
+
+    let body = JSON.stringify({fk_persona:{pk_cedula: cedula,i_telefono: telefono,v_direccion: direccion,
+                              username: nusuario,email:email,first_name:nombres,last_name:apellidos,password:contrasena}});
+    
+    return this.httpClient.post<any>("http://127.0.0.1:8000/api/v1/usuarios/clientes/",body,{headers:myheaders})
+  }
+}
