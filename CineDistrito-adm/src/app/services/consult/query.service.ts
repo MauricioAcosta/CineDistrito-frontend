d858868591318ccb7a0e09d3263c8170a853434e
MauricioAcosta <andres.taamap@gmail.com>
2019-07-24 11:10:27 -0500

    mejora de atributos

--- CineDistrito-adm/src/app/services/consult/query.service.ts
+++ CineDistrito-adm/src/app/services/consult/query.service.ts
@@ -4,7 +4,6 @@ import { Personas } from 'src/app/models/personas';
 import { Peliculas } from 'src/app/models/peliculas';
 import { Contratos } from 'src/app/models/contratos';
 import { Time } from '@angular/common';
-import { Salas } from 'src/app/models/salas';
 import { Multiplex } from 'src/app/models/multiplex';
 
 @Injectable({
@@ -19,16 +18,16 @@ export class QueryService {
     myheaders = myheaders.append("Authorization", "Basic " + btoa("admin:admin123"));
     myheaders = myheaders.append("Content-Type", "application/x-www-form-urlencoded");
     const httpOptions = { headers: myheaders };
-    return this.httpClient.get<Personas>('http://localhost:8000/api/v1/usuarios/personas', httpOptions);
+    return this.httpClient.get<Personas>('http://192.168.43.110:8080/api/v1/usuarios/personas', httpOptions);
   }
   public GetPeliculas() {
-    return this.httpClient.get<Peliculas>('http://localhost:8000/api/v1/funciones/peliculas/');
+    return this.httpClient.get<Peliculas>('http://192.168.43.110:8080/api/v1/funciones/peliculas/');
   }
   public GetContratos() {
-    return this.httpClient.get<Contratos>('http://localhost:8000/api/v1/empleados/contratos/');
+    return this.httpClient.get<Contratos>('http://192.168.43.110:8080/api/v1/empleados/contratos/');
   }
   public PostCreateUser(pk_cedula: Number, i_telefono: Number, v_direccion: String, username: String, email: String, firt_name: String, last_name: String, password: String, n_descuento: Number, fk_numcontrato: Number) {
-    this.httpClient.post("http://127.0.0.1:8000/api/v1/empleados/empleados/",
+    this.httpClient.post("http://192.168.43.110:8080/api/v1/empleados/empleados/",
       {
         "fk_persona": {
           "pk_cedula": pk_cedula,
@@ -58,10 +57,10 @@ export class QueryService {
   }
 
   public GetFuncion() {
-    return this.httpClient.get<Funciones>('http://localhost:8000/api/v1/funciones/funciones/')
+    return this.httpClient.get<Funciones>('http://192.168.43.110:8080/api/v1/funciones/funciones/')
   }
   public PostCreateFuntion(v_estado: String, d_proyeccion: Date, fk_pelicula: Number, t_inicioproyeccion: Time, t_finproyeccion: Time) {
-    this.httpClient.post("http://localhost:8000/api/v1/funciones/funciones/",
+    this.httpClient.post("http://192.168.43.110:8080/api/v1/funciones/funciones/",
       {
         "v_estado": v_estado,
         "d_proyeccion": d_proyeccion,
@@ -72,14 +71,14 @@ export class QueryService {
         data => { console.log("POST Request is successful ", data); }, error => { console.log("Error feo", error); });
   }
   public GetSalas() {
-    return this.httpClient.get<Multiplex>('http://localhost:8000/api/v1/multiplex/multiplex/')
+    return this.httpClient.get<Multiplex>('http://192.168.43.110:8080/api/v1/multiplex/multiplex/')
   }
   public GetCreateSala() {
-    return this.httpClient.get("http://localhost:8000/api/v1/funciones/funciones-sala/")
+    return this.httpClient.get("http://192.168.43.110:8080/api/v1/funciones/funciones-sala/")
   }
 
   public PostCreateSala(fk_funcion: Number, fk_sala: Number) {
-    this.httpClient.post("http://localhost:8000/api/v1/funciones/funciones-sala/",
+    this.httpClient.post("http://192.168.43.110:8080/api/v1/funciones/funciones-sala/",
       {
         "fk_funcion": fk_funcion,
         "fk_sala": fk_sala
@@ -87,7 +86,7 @@ export class QueryService {
         data => { console.log("POST Request is successful ", data); }, error => { console.log("Error feo", error); });
   }
   public PostCreateSnacks(v_tipo: String, v_nombre: String, tx_descripcion: String, i_precio: Number, i_puntosofrecidos: Number) {
-    this.httpClient.post("http://127.0.0.1:8000/api/v1/snacks/snacks/",
+    this.httpClient.post("http://192.168.43.110:8080/api/v1/snacks/snacks/",
       {
         "v_tipo": v_tipo,
         "v_nombre": v_nombre,
